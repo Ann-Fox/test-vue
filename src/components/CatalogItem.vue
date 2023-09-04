ba55425e9512103c92c3af532d016e46f4070f86
Ann-Fox <fx8565@mail.ru>
2023-09-04 19:04:41 +0500

    layout Catalog Item

--- src/components/CatalogItem.vue
+++ src/components/CatalogItem.vue
@@ -1,8 +1,52 @@
 <script setup>
 // import WelcomeItem from './WelcomeItem.vue'
+// import { storeToRefs } from 'pinia';
+// import { ref } from 'vue';
+// import { useCounterStore } from '../stores/counter'
+
+const props = defineProps ({
+nameProduct: {
+  type: String,
+  required: true
+},
+priceProduct: {
+  type: Number,
+  required: true
+}
+})
+
+// const store = useCounterStore()
+
+
 
 </script>
 
 <template>
-  <h1>CatalogItem</h1>
+<section class="product-item">
+  <div class="name-price">
+    <div>{{ nameProduct }}</div>
+    <div>{{ priceProduct }}</div>
+  </div>
+  <div class="action">
+    <button>calc</button>
+    <input type="number">
+    <button>+</button>
+    <button>-</button>
+    <button>basket</button>
+  </div>
+</section>
 </template>
+
+<style scoped>
+input {
+  max-width: 50px;
+}
+.product-item {
+  display: flex;
+  gap: 20px;
+}
+.name-price {
+  display: flex;
+  gap: 20px;
+}
+</style>
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,28 +1,28 @@
 <script setup>
 import CatalogItem from '../components/CatalogItem.vue'
 
-// const products = [
-// {
-//     name: 'Продукт 1',
-//     price: 333.33
-//   },
-//   {
-//     name: 'Продукт 2',
-//     price: 444.44
-//   },
-//   {
-//     name: 'Продукт 3',
-//     price: 555.55
-//   },
-//   {
-//     name: 'Продукт 4',
-//     price: 222.22
-//   },
-//   {
-//     name: 'Продукт 5',
-//     price: 111.11
-//   }
-// ]
+const products = [
+{
+    name: 'Продукт 1',
+    price: 333.33
+  },
+  {
+    name: 'Продукт 2',
+    price: 444.44
+  },
+  {
+    name: 'Продукт 3',
+    price: 555.55
+  },
+  {
+    name: 'Продукт 4',
+    price: 222.22
+  },
+  {
+    name: 'Продукт 5',
+    price: 111.11
+  }
+]
 </script>
 
 <template>
@@ -32,6 +32,16 @@ import CatalogItem from '../components/CatalogItem.vue'
       <h3>Price</h3>
       <h3>Action</h3>
     </section>
-    <CatalogItem />
+    <section class="catalog">
+      <CatalogItem v-for="product in products" :key="product.id" :name-product = 'product.name' :price-product="product.price">
+      </CatalogItem>
+    </section>
   </main>
 </template>
+
+<style>
+.head {
+  display: flex;
+  gap: 50px;
+}
+</style>
